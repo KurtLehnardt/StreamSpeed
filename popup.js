19b2cfee9af59033e385aca8cba1ae94f002934f
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2022-05-16 00:50:41 -0700

    transition easein and out

--- popup.js
+++ popup.js
@@ -15,7 +15,7 @@ chrome.tabs.executeScript({
     var source
     if (document.location.href.includes('kanopy')){
         source = 'kanopy'
-        container = document.getElementsByClassName('vjs-tech')[0]
+        container = document.getElementById('vjs_video_3')
     } else if (document.location.href.includes('youtube')){
         source = 'youtube'
         container = document.getElementsByClassName('style-scope ytd-video-primary-info-renderer')[0]
@@ -33,77 +33,77 @@ chrome.tabs.executeScript({
         container = document.getElementsByClassName('player-fullscreen-wrapper')[0]
     }
     
-    var slider = document.getElementById('speedSlider');
+    var slider = document.getElementById('speedSlider')
     var video
     if (source === 'amazon'){
         vid_elem = document.getElementsByTagName('video')
         video = vid_elem[vid_elem.length-1]
     } else {
-        video = document.getElementsByTagName('video')[0];
+        video = document.getElementsByTagName('video')[0]
     }
     if (!slider){
-        var br = document.createElement('br');
+        var br = document.createElement('br')
 
-        var div = document.createElement('div');
-        div.id = 'sliderContainer';
+        var div = document.createElement('div')
+        div.id = 'sliderContainer'
         if (source === 'youtube') {
-            div.style.cssText = 'position: relative; margin: 0 auto 3rem';
+            div.style.cssText = 'position: relative; margin: 0 auto 3rem;'
         } else if (source === 'netflix'){
-            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999'
+            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999;'
         } else if (source === 'amazon'){
-            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999; width: 100%'
+            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999; width: 100%;'
         } else if (source === 'hbo' || source === 'kanopy'){
-            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999'
+            div.style.cssText = 'position: relative; margin: 0px auto 3rem; z-index: 9999;'
         } else if (source === 'acloudguru'){
-            div.style.cssText = 'position: absolute; margin: 0px auto 3rem; z-index: 9999; width: 100%'
+            div.style.cssText = 'position: absolute; margin: 0px auto 3rem; z-index: 9999; width: 100%;'
         }
+        
 
-
-        var title = document.createElement('span');
-        title.id = 'sliderTitle';
-        title.innerText = 'Change video speed';
+        var title = document.createElement('span')
+        title.id = 'sliderTitle'
+        title.innerText = 'Change video speed'
         title.style.cssText = 'color: rgb(221, 149, 15); font-size: 1.5em; text-align: center;'
 
-        var sliderLabel = document.createElement('output');
-        sliderLabel.id = 'sliderLabel';
-        sliderLabel.innerText = '1';
+        var sliderLabel = document.createElement('output')
+        sliderLabel.id = 'sliderLabel'
+        sliderLabel.innerText = '1'
         sliderLabel.style.cssText = 'position: absolute; background-color: rgb(221, 149, 15); color: white; font-size: 1.5em; text-align: center; padding: 3px 8px; top: 105%;'
 
-        var resetButton = document.createElement('button');
-        resetButton.id = 'resetButton';
+        var resetButton = document.createElement('button')
+        resetButton.id = 'resetButton'
         resetButton.innerText = 'Reset'
         resetButton.style.cssText = 'float: right; color: rgb(221, 149, 15); background: none; font-size: 1.5em; text-align: center; border: 1px solid grey; border-radius: 1px; margin-bottom: 3px;'
 
-        range = document.createElement('input');
-        range.type = 'range';
-        range.id = 'speedSlider';
-        range.setAttribute('min', '0.1');
-        range.setAttribute('max', '10');
-        range.setAttribute('step', '0.1');
-        range.setAttribute('value', '1');
+        range = document.createElement('input')
+        range.type = 'range'
+        range.id = 'speedSlider'
+        range.setAttribute('min', '0.1')
+        range.setAttribute('max', '10')
+        range.setAttribute('step', '0.1')
+        range.setAttribute('value', '1')
         range.style.cssText = '-webkit-appearance: none; background-color: rgb(221, 149, 15); opacity: 0.9; width: 100%;'
 
-        div.prepend(br);
-        div.appendChild(title);
-        div.appendChild(resetButton);
-        div.appendChild(sliderLabel);
-        div.appendChild(br);
-        div.appendChild(range);
-        div.appendChild(br);
-        container.prepend(div);
+        div.prepend(br)
+        div.appendChild(title)
+        div.appendChild(resetButton)
+        div.appendChild(sliderLabel)
+        div.appendChild(br)
+        div.appendChild(range)
+        div.appendChild(br)
+        container.prepend(div)
     } 
 
-    slider = document.getElementById('speedSlider');
-    var resetButton = document.getElementById('resetButton');
+    slider = document.getElementById('speedSlider')
+    var resetButton = document.getElementById('resetButton')
 
     function updateSpeed(){
-        video.playbackRate = slider.value;
+        video.playbackRate = slider.value
         updateSliderLabel(video.playbackRate)
     }
 
     function resetSpeed(){
-        slider.value = 1.0;
-        video.playbackRate = 1;
+        slider.value = 1.0
+        video.playbackRate = 1
         updateSliderLabel(video.playbackRate)
     }
 
@@ -111,23 +111,29 @@ chrome.tabs.executeScript({
         if (source !== 'youtube') {
             showAndHideSlider()
         }
-        var sliderLabel = document.getElementById('sliderLabel');
-        var currentSpeed = speed.toString();
+        var sliderLabel = document.getElementById('sliderLabel')
+        var currentSpeed = speed.toString()
         if (currentSpeed.length === 1){
-            currentSpeed = currentSpeed + '.0';
+            currentSpeed = currentSpeed + '.0'
         }
         var sliderVal = speed * 10
-        sliderLabel.innerText = currentSpeed;
-        sliderLabel.style.left = 'calc(' + sliderVal + '% + (' + (8 - sliderVal * 0.4) + 'px))';
+        sliderLabel.innerText = currentSpeed
+        sliderLabel.style.left = 'calc(' + sliderVal + '% + (' + (8 - sliderVal * 0.4) + 'px))'
     }
 
     var fadingOut = false
     function showAndHideSlider(){
         var sliderContainer = document.getElementById('sliderContainer')
+        // TODO fix this ease in/out
+        sliderContainer.style.cssText = sliderContainer.style.cssText.split('transition: .5s ease-out;')[0]
+        sliderContainer.style.cssText += 'transition: .5s ease-in;'
         sliderContainer.style.display = 'block'
         if (!fadingOut) {
+            sliderContainer.style.cssText = sliderContainer.style.cssText.split('transition: .5s ease-in;')[0]
+            div.style.cssText += 'transition: .5s ease-out;'
             var fadeOut = setTimeout(()=>{
-                sliderContainer.style.display = 'none'; fadingOut = false;
+                sliderContainer.style.display = 'none'
+                fadingOut = false
             }, 3000)
             fadingOut = true
             }
